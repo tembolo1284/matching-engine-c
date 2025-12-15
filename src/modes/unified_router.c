f836694db7f4ac7b0ab8b4f5a1c32d093c640511
Paul Nikholas Lopez <nik.lopez381@gmail.com>
2025-12-14 20:05:52 -0500

    Added errno.h to unified.router.c

--- src/modes/unified_router.c
+++ src/modes/unified_router.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <errno.h>
 
 /* ============================================================================
  * Send to Multicast
